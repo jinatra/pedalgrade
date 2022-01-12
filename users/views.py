37d22f1043b03e32efa2c52bc0701610dfe1eb77
jinatra <jinatra0816@gmail.com>
2022-01-12 12:02:34 +0900

    EDIT: 그레이드 메일

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from django.http      import JsonResponse
+
+from users.models import GradeUser, PedalUser
+
+class GradeMailSubscribeView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            email = data['email']
+
+            if GradeUser.objects.filter(email=email).exists():
+                return JsonResponse({'MESSAGE':'existing_user'}, status=400)
+
+
+        
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
+
