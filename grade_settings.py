dc96c8ec492c995e794fc0ae6968d1a1f4d0c247
jinatra <jinatra0816@gmail.com>
2022-01-12 11:25:15 +0900

    ADD: Initial Setting

---
+++ grade_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'grade',
+        'USER': 'root',
+        'PASSWORD': 'WJpedal0103!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-f4l2q4xhyucq6o-940u4e)j6y5m*9^rsm544=vq7x7eibrywr3'
