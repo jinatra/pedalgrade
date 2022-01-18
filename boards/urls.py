80ed8e88748ce3fcfab15bf320901ffbf2a0caff
jinatra <jinatra0816@gmail.com>
2022-01-18 12:18:53 +0900

    ADD: MainPageView

--- boards/urls.py
+++ boards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from boards.views import BoardCreateView, BoardReadView
+from boards.views import BoardCreateView, BoardReadView, MainPageView
 
 urlpatterns = [
     path('', BoardCreateView.as_view()),
-    path('/<int:board_id>', BoardReadView.as_view() )
+    path('/<int:board_id>', BoardReadView.as_view()),
+    path('/list', MainPageView.as_view()),
 ]
